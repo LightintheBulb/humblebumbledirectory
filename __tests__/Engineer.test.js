0bd2588f9a7f985c837d9d8b3cb874390ee075ee
Lightinthebulb <>
2022-11-22 19:34:35 -0500

    tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,13 @@
+const Engineer = require("../lib/Engineer");
+
+test("Can set GitHUb account via constructor", () => {
+  //TODO:code goes here
+});
+
+test("getRole() should return \"Engineer\"", () => {
+  //TODO:code goes here
+});
+
+test("Can get GitHub username via getGithub()", () => {
+  //TODO:code goes here
+});
---
+++ __tests__/Intern.test.js
@@ -0,0 +1,15 @@
+const Intern = require("../lib/Intern");
+
+test("Can set school via constructor", () => {
+  const testValue = "UCLA";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+  //TODO:code goes here
+});
+
+test("Can get school via getSchool()", () => {
+  //TODO:code goes here
+});
